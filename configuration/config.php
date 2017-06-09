bfef3decaaee1c836f24d88eebba022cb02ad142
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-06-09 15:55:13 +0700

    update

--- configuration/config.php
+++ configuration/config.php
@@ -15,8 +15,8 @@ define('APP_CORP'   , 'THE MEDIA HOTEL AND TOWERS');
 define('APP_VERSION', '0.1');
 define('APP_YEAR'   , '2017');
 define('LICENSE_TO' , 'THE MEDIA HOTEL AND TOWERS');
-define('REPORT_BIRT','http://103.43.47.115:8888/birt/frameset?__report=report/pos/');
-define('BIRT','http://103.43.47.115:8888/birt/output?__report=report/pos/struk_order.rptdesign&__format=pdf');
+define('REPORT_BIRT','http://localhost:8080/birt/frameset?__report=report/pos/');
+define('BIRT','http://localhost:8080/birt/output?__report=report/pos/struk_order.rptdesign&__format=pdf');
 // -- Module
 define('DEF_MODULE'     , 1); // 1. perencanaan 2.etc  ref => apps table
 define('SELECT_MODULE'  , TRUE);
