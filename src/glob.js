c973549ad8f38cf63c6e4502509773317df7eb7f
Afa Rappresent <mail.vudin@gmail.com>
2017-06-15 12:16:35 +0700

    improve session

--- src/glob.js
+++ src/glob.js
@@ -14,14 +14,10 @@ const getIp = function () {
 };
 module.exports = {
     env: env,
-    token: {
-        header: 'X-Token',
-        expires: parseInt(ENV.TOKEN_EXPIRES) || 15 * 60 * 1000, //default to 15 minutes
-        extend: ENV.TOKEN_EXTEND || true,
-        regen: ENV.TOKEN_REGEN || false,
-        resave: ENV.TOKEN_RESAVE || true //if false regen value will force to true
+    session: {
+        maxAge: parseInt(ENV.MAX_AGE) || 15 * 60 * 1000, //default to 15 minutes,
+        resave: true,
     },
-    regenToken: true,
     reqTimeOut: parseInt(eval(ENV.npm_package_reqTimeOut)) || 1 * 60 * 1000,
     name: ENV.NAME || JSON.parse(fs.readFileSync(`${home}/package.json`)).name,
     description: ENV.npm_package_description || JSON.parse(fs.readFileSync(`${home}/package.json`)).description,
@@ -35,14 +31,5 @@ module.exports = {
         uploadDir: `${home}/uploads`,
         maxFilesSize: 5000000,
         fieldName: 'uploads',
-    },
-    maxPaging: 10000,
-    regEx: {
-        //email : /[a-z0-9]+[_a-z0-9\.-]*[a-z0-9]+@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/i,
-        email: /^(?:(?:[\w`~!#$%^&*\-=+;:{}'|,?\/]+(?:(?:\.(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)*"|[\w`~!#$%^&*\-=+;:{}'|,?\/]+))*\.[\w`~!#$%^&*\-=+;:{}'|,?\/]+)?)|(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)+"))@(?:[a-zA-Z\d\-]+(?:\.[a-zA-Z\d\-]+)*|\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])$/i,
-        phone: /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/i,
-        username: /^[a-zA-Z0-9]*$/i,
-        password: /[a-zA-Z0-9\_\~\!\@\#\$\%\^\&\*\(\)\_\+\`\-\=\[\]\\\{\}\|\;\'\:\"\,\.\/\<\>\?]+/i,
-        zipcode: /(^\d{5}([\-]?\d{4})?$)|(^(GIR|[A-Z]\d[A-Z\d]??|[A-Z]{2}\d[A-Z\d]??)[ ]??(\d[A-Z]{2})$)|(\b((?:0[1-46-9]\d{3})|(?:[1-357-9]\d{4})|(?:[4][0-24-9]\d{3})|(?:[6][013-9]\d{3}))\b)|(^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$)|(^(F-)?((2[A|B])|[0-9]{2})[0-9]{3}$)|(^(V-|I-)?[0-9]{5}$)|(^(0[289][0-9]{2})|([1345689][0-9]{3})|(2[0-8][0-9]{2})|(290[0-9])|(291[0-4])|(7[0-4][0-9]{2})|(7[8-9][0-9]{2})$)|(^[1-9][0-9]{3}\s?([a-zA-Z]{2})?$)|(^([1-9]{2}|[0-9][1-9]|[1-9][0-9])[0-9]{3}$)|(^([D-d][K-k])?( |-)?[1-9]{1}[0-9]{3}$)|(^(s-|S-){0,1}[0-9]{3}\s?[0-9]{2}$)|(^[1-9]{1}[0-9]{3}$)|(^\d{6}$)/i
     }
 };
--- src/http.js
+++ src/http.js
@@ -1,22 +1,21 @@
 const fs = require('fs'),
     path = require('path'),
     ejs = require('ejs'),
-    bluebird = require('bluebird'),
+    Promise = require('bluebird'),
     express = require('express'),
+    cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
-    mysql = require('mysql'),
-    uuid = require('node-uuid'),
-    dateformat = require('dateformat'),
-    bcrypt = require('bcrypt');
+    mysql = require('promise-mysql');
 //
 const {STATUS_CODES} = require('http'),
     Glob = require(`./glob`),
     httpCode = require(`${Glob.home}/utils/http.code`),
     Code4 = require(`${Glob.home}/utils/code4`),
+    Crypt = require(`${Glob.home}/utils/crypt`),
     Upload = require(`${Glob.home}/utils/upload`)(Glob.upload);
 //
-const {env, name, description, version, ip, port, home, factory, config, regEx} = Glob;
+const {env, name, description, version, session, port, home, factory, config, regEx} = Glob;
 const Db = mysql.createPool(config.mysql);
 const compileQuery = function (sql, data, cb) {
     Db.getConnection(function (err, conn) {
@@ -38,15 +37,19 @@ const compileQuery = function (sql, data, cb) {
         }
     });
 };
-const http = function () {
+const http = function (pool, compile) {
+    let del = 'password,default_module,default_menu,status,created_date,modified_date,created_by,modified_by';
     let app = express();
     let locals = app.locals;
-    let Auth = require(`${Glob.home}/src/auth`);
+    let cookie = {
+        path: '/',
+        maxAge: session.maxAge,
+        httpOnly: true
+    };
     //
-    locals.logged = false;
-    locals.name = Glob.name;
-    locals.version = Glob.version;
-    locals.description = Glob.description;
+    locals.name = name;
+    locals.version = version;
+    locals.description = description;
     //
     /** **************************************************************************
      ** express.js setup
@@ -60,6 +63,7 @@ const http = function () {
     /** **************************************************************************
      ** commonly middleware setup
      ** **************************************************************************/
+    app.use(cookieParser())
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(express.static(path.join(Glob.home, 'public')));
@@ -68,11 +72,9 @@ const http = function () {
         next();
     });
     if (app.get('env') === 'production') {
+        cookie.secure = true;
         app.set('trust proxy', 1);
     }
-    /** **************************************************************************
-     ** authentication as middleware
-     ** **************************************************************************/
     app.use(async function (req, res, next) {
         let url = req._parsedUrl.pathname;
         let {httpVersion, method, headers, params, query, body} = req;
@@ -81,81 +83,99 @@ const http = function () {
             if (!Object.keys(request[s]).length) delete request[s];
         });
         //
-        //console.log(process.pid.toString(), '>', JSON.stringify(request));
-        console.log(process.pid.toString(), '>', JSON.stringify({url, method}));
+        console.log(process.pid.toString(), '>', JSON.stringify(request));
         next();
     });
-    /** **************************************************************************
-     ** authentication : http request
-     ** **************************************************************************/
-    app.get('/', function (req, res, next) {
-        if (locals.logged) return res.render('home');
+    app.use(async function (req, res, next) {
+        let myCookie = req.cookies[name];
+        let error = new Error('Invalid cookie!');
+        let pathname = req._parsedUrl.pathname, method = req.method;
+        let isLoginPage = pathname === '/login' && method === 'GET';
+        let isAuthing = pathname === '/auth' && method === 'POST';
+        //
+        if (myCookie) {
+            myCookie = Crypt.de(myCookie);
+            try {
+                let {id, time} = JSON.parse(myCookie);
+                let getUserQuery = `SELECT * FROM user WHERE id = ${id}`;
+                let getter = await compile(getUserQuery);
 
-        res.redirect('/login');
+                time += cookie.maxAge;
+
+                if (getter.constructor == Error) throw getter;
+                if (time < new Date().getTime()) throw error;
+                if (isLoginPage || isAuthing) return res.redirect('/');
+
+                del.split(',').forEach(function(key){
+                    delete getter[0][key]
+                });
+                locals.user = getter[0];
+                locals.message = '';
+                return next()
+            } catch (e) {
+                if (isLoginPage || isAuthing) return next();
+                return res.redirect('/login');
+            }
+        }
+        if (isLoginPage || isAuthing) return next();
+        return res.redirect('/login');
+    });
+    app.get('/', function (req, res, next) {
+        res.render('home')
     });
     app.get('/order', function (req, res, next) {
-        if (locals.logged) return res.render('order');
-
-        res.redirect('/login');
+        res.render('order')
     });
     app.get('/login', function (req, res, next) {
-        if (locals.logged) {
-            delete locals.message;
-            return res.redirect('/');
-        }
-
-        locals.logged = false;
         locals.message = locals.message || "Please fill form input..";
+        res.clearCookie(name);
         res.render('login');
     });
     app.get('/logout', async function (req, res, next) {
-        if (locals.logged) {
-            let {id, username} = locals.logged;
-            let updateUser = 'UPDATE user SET token = NULL WHERE id = ?';
-            compileQuery(updateUser, id, function (e, rows, fields) {
-                if (e) {
-                    locals.message = `Cannot destroy token for ${username} account!`;
-                    res.redirect('/');
-                } else {
-                    locals.logged = false;
-                    res.redirect('/login')
-                }
-            });
+        res.clearCookie(name);
+        res.redirect('/login')
+    });
+    app.post('/sql', async function (req, res, next) {
+        let {query, params} = req.body;
+        let data = [], error = null, result = await compile(query, params);
+        if (result.constructor == Error) {
+            error = result;
         } else {
-            res.redirect('/login')
+            data = result
         }
-
+        res.send({error, data});
     });
     app.post('/auth', async function (req, res, next) {
-        if (locals.logged) return res.redirect('/');
-
         let {body} = req, {username, password} = body;
-        let selectUser = `SELECT * FROM user WHERE name = '${username}'`;
         //
-        compileQuery(selectUser, 0, function (e, rows, fields) {
-            if (e || !rows.length) {
-                locals.message = `Invalid username "${username}"!`;
-                res.redirect('/login');
-            } else if (rows.length) {
-                let user = rows[0];
-                if (user.password !== password) {
-                    locals.message = `Invalid password "${password}" for ${username} account!`;
-                    res.redirect('/login');
-                } else {
-                    let token = uuid.v4();
-                    let updateUser = 'UPDATE user SET token = ? WHERE id = ?';
-                    compileQuery(updateUser, [token, user.id], function (e, rows, fields) {
-                        if (e) {
-                            locals.message = `Cannot tokenize for ${username} account!`;
-                            res.redirect('/login');
-                        } else {
-                            locals.logged = {id: user.id, username, token};
-                            res.redirect('/');
-                        }
-                    })
-                }
-            }
-        });
+        try {
+            let getUserQuery = `SELECT * FROM user WHERE name = '${username}'`;
+            let getter = await compile(getUserQuery);
+
+            if (getter.constructor == Error) throw getter;
+            if (getter.length !== 1) throw new Error(`Invalid username "${username}"!`);
+            if (getter[0].password !== password) throw new Error(`Invalid password "${password}" for ${username} account!`);
+
+            let {id} = getter[0];
+            let time = new Date().getTime();
+            let token = Crypt.en(JSON.stringify({id, time}));
+            let updateUserQuery = 'UPDATE user SET token = ? WHERE id = ?';
+            let setter = await compile(updateUserQuery, [token, id]);
+
+            if (setter.constructor == Error) throw setter;
+
+            cookie.expire = time + cookie.maxAge;
+            del.split(',').forEach(function(key){
+                delete getter[0][key]
+            });
+            locals.user = getter[0];
+            locals.message = '';
+            res.cookie(name , token, cookie);
+            res.redirect('/');
+        } catch (e) {
+            locals.message = e.message;
+            res.redirect('/login');
+        }
     });
     /** **************************************************************************
      ** error handling : http request
@@ -191,12 +211,32 @@ const http = function () {
             }
         }
         res.status(locals.ERR.status);
-        res.json(locals.ERR);
+        if (req.xhr || req.headers.accept.indexOf('json') > -1) {
+            res.json(locals.ERR);
+        } else {
+            res.render('error')
+        }
     });
     //
     return app;
 };
 
 module.exports = async function () {
-    return http();
+    let pool = await mysql.createPool(config.mysql);
+    let compile = async function (query, data) {
+        try {
+            let conn = await pool.getConnection();
+            if (!conn) throw new Error(conn);
+
+            let result = await conn.query(query, data);
+            if (!result) throw result;
+
+            pool.releaseConnection(conn);
+
+            return result;
+        } catch (e) {
+            return e
+        }
+    };
+    return http(pool, compile);
 };
