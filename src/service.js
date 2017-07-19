2d692e5aa7c6df427d8368d724fbb31288d7b13f
Afa Rappresent <mail.vudin@gmail.com>
2017-07-20 01:27:39 +0700

    add nodejs as a service, for windows os

---
+++ src/service.js
@@ -0,0 +1,15 @@
+let nodeWindows = require('node-windows').Service;
+let service = new nodeWindows({
+	name:'POS',
+	description: 'POS standalone',
+	script: dirname__ + '\\bin\\app'
+});
+service.on('install',function(){
+	service.start();
+});
+service.on('uninstall',function(){
+	console.log('Uninstall complete.');
+	console.log('The service exists: ',service.exists);
+});
+//
+module.exports = service;
